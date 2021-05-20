6e8466416c9a435c602dc6e39791550b7e028a5f
Yukti Garg <Yukti.Garg@sita.aero>
2021-05-20 12:02:38 +0530

    Add project files.

---
+++ BloggingApplication/Models/BlogContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BloggingApplication.Models
+{
+    public partial class BlogContext : DbContext
+    {
+        
+        public BlogContext(DbContextOptions<BlogContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blogs> Blogs { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blogs>(entity =>
+            {
+                entity.HasKey(e => e.BlogId)
+                    .HasName("PK__Blogs__54379E3075E9BDFA");
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Content)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4C4FC41C49");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ BloggingApplication/Models/Blogs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BloggingApplication.Models
+{
+    public partial class Blogs
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public int Shares { get; set; }
+    }
+}
